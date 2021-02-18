f356adf51a12a72096f984bbe159aaf2331ad966
Alvaro Gutierrez Valle <alvarogtv05@gmail.com>
2021-02-18 15:03:24 +0100

    remove one file

--- flight_data.py
+++
@@ -1,7 +0,0 @@
-API_KEY='vbwJ3h20T1PChgAoYyQKmPy1_LfCECAQ'
-
-TEQUILA_ENDPOINT= "https://tequila-api.kiwi.com/"
-
-class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
